5684d18b44a89f0fd9738725395f6873d2458731
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-03-10 11:26:58 +0000

    UPDATE - Create github actions pipeline

--- importmanualbucket.tf
+++ importmanualbucket.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "manual_bucket" {
-  # (resource arguments)
-}
+# resource "aws_s3_bucket" "manual_bucket" {
+#   # (resource arguments)
+# }
 
-resource "aws_s3_bucket_versioning" "s3versioningmanual" {
-  bucket = aws_s3_bucket.manual_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "s3versioningmanual" {
+#   bucket = aws_s3_bucket.manual_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
