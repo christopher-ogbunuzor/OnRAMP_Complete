4487bb83ffd881c92dd3c2d28e4dadc93decc985
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-03-10 14:57:02 +0000

    TEST - check if pipeline succeeds

--- output.tf
+++
@@ -1,3 +0,0 @@
-output "public_server_ip" {
-  value = aws_instance.public_server.public_ip
-}
--- terraformfiles/ec2.tf
+++ terraformfiles/ec2.tf
@@ -3,7 +3,7 @@ data "aws_ami" "my_aws_ami" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami*"]
+    values             = ["amzn2-ami*"]
   }
   owners      = ["137112412989"]
   most_recent = true
